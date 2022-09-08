0dfc38b6052ffaa028de985e1705fecd6de7bb9a
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-09-08 13:37:28 +0530

    Repository Pattern Implemented

---
+++ HotelListing.API/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelListing.API.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int? id);
+        Task<List<T>> GetAllAsync();
+        Task<T> Addsync(T entity);
+        Task DeleteSync(int id);
+        Task UpdateAsync(T entity);
+        Task<bool> Exists(int id);
+    }
+}
